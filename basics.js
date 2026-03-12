f7aff7e10d7133b1e4ab8479ac689f53cd7b2c8c
Ragul priyan <ragulpriyan2731@gmail.com>
2026-03-12 20:32:39 +0530

    javascrits

---
+++ basics.js
@@ -0,0 +1,48 @@
+/*//VARIABLE
+var Name = "ragul";
+console.log(Name)
+let age = 27;
+console.log(age)
+const MyName = "sha";
+console.log(MyName);
+//what's the difference between var and let
+// var can be reasign a variable with same name it possible!
+var Name = "ragul"
+console.log(Name)
+var Name = "sha"
+console.log(Name)
+// let can't be reasign a variable with same name it's not possible
+let Summit = 30;
+console.log(Summit)
+let Summit = 40
+console.log(Summit)
+//what is const
+//const is constant it can't be redeclared a variable value
+const Message = "i am fine"
+console.log(Message)
+//DATATYPE
+//primitive datatype and non-primitive datatype
+//primitive datatype
+let Employee ="ragul" //String
+console.log(Employee)
+var age = 27 //Number 
+console.log(age)
+const Passing = true //Boolean
+console.log(Passing)
+let Value = null //null
+console.log(Value)
+let Student ; // undefined
+console.log(Student)
+//non-primitive datatype
+let Myarray = ["ragul","sha","indhuja","25","27"] //Array
+console.log(Myarray)
+console.log(Myarray[2])*/
+let Product = {
+    Name:science,
+    Age:27,
+}
+console.log(Product.Name) //objects
+                                                
+//function
+
+
